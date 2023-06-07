20f92284c5128891d3f840444a129e112d8914de
Ajil Oommen <ajil.oommen@egovernments.org>
2023-06-07 21:35:02 +0530

    [hlm-000]: Add showcase base

--- apps/health_campaign_field_worker_app/lib/pages/authenticated.dart
+++ apps/health_campaign_field_worker_app/lib/pages/authenticated.dart
@@ -6,6 +6,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_portal/flutter_portal.dart';
 import 'package:isar/isar.dart';
 import 'package:location/location.dart';
+import 'package:showcaseview/showcaseview.dart';
 
 import '../blocs/boundary/boundary.dart';
 import '../blocs/household_details/household_details.dart';
@@ -32,132 +33,137 @@ class AuthenticatedPageWrapper extends StatelessWidget {
         final showDrawer = snapshot.data ?? false;
 
         return Portal(
-          child: Scaffold(
-            appBar: AppBar(
-              actions: [
-                BlocBuilder<BoundaryBloc, BoundaryState>(
-                  builder: (ctx, state) {
-                    final selectedBoundary = ctx.boundaryOrNull;
+          child: ShowCaseWidget(
+            builder: Builder(builder: (context) {
+              return Scaffold(
+                appBar: AppBar(
+                  actions: [
+                    BlocBuilder<BoundaryBloc, BoundaryState>(
+                      builder: (ctx, state) {
+                        final selectedBoundary = ctx.boundaryOrNull;
 
-                    if (selectedBoundary == null) {
-                      return const SizedBox.shrink();
-                    }
+                        if (selectedBoundary == null) {
+                          return const SizedBox.shrink();
+                        }
 
-                    final boundaryName = selectedBoundary.name ??
-                        selectedBoundary.code ??
-                        'No boundary name';
+                        final boundaryName = selectedBoundary.name ??
+                            selectedBoundary.code ??
+                            'No boundary name';
 
-                    final theme = Theme.of(context);
+                        final theme = Theme.of(context);
 
-                    return TextButton(
-                      style: TextButton.styleFrom(
-                        foregroundColor: theme.colorScheme.onPrimary,
-                      ),
-                      onPressed: () {
-                        ctx.router.navigate(const BoundarySelectionRoute());
-                      },
-                      child: Text(boundaryName),
-                    );
-                  },
-                ),
-              ],
-            ),
-            drawer: showDrawer ? const Drawer(child: SideBar()) : null,
-            body: MultiBlocProvider(
-              providers: [
-                BlocProvider(
-                  create: (context) {
-                    return SearchHouseholdsBloc(
-                      beneficiaryType: context.beneficiaryType,
-                      userUid: context.loggedInUserUuid,
-                      projectId: context.projectId,
-                      projectBeneficiary: context.repository<
-                          ProjectBeneficiaryModel,
-                          ProjectBeneficiarySearchModel>(),
-                      householdMember: context.repository<HouseholdMemberModel,
-                          HouseholdMemberSearchModel>(),
-                      household: context
-                          .repository<HouseholdModel, HouseholdSearchModel>(),
-                      individual: context
-                          .repository<IndividualModel, IndividualSearchModel>(),
-                      taskDataRepository:
-                          context.repository<TaskModel, TaskSearchModel>(),
-                    )..add(const SearchHouseholdsClearEvent());
-                  },
-                ),
-                BlocProvider(
-                  create: (context) {
-                    final userId = context.loggedInUserUuid;
-
-                    final isar = context.read<Isar>();
-                    final bloc = SyncBloc(
-                      isar: isar,
-                      networkManager: context.read(),
-                    )..add(SyncRefreshEvent(userId));
-
-                    isar.opLogs
-                        .filter()
-                        .createdByEqualTo(userId)
-                        .syncedUpEqualTo(false)
-                        .watch()
-                        .listen(
-                      (event) {
-                        bloc.add(
-                          SyncRefreshEvent(
-                            userId,
-                            event.where((element) {
-                              switch (element.entityType) {
-                                case DataModelType.household:
-                                case DataModelType.individual:
-                                case DataModelType.task:
-                                case DataModelType.householdMember:
-                                case DataModelType.projectBeneficiary:
-                                case DataModelType.stock:
-                                case DataModelType.stockReconciliation:
-                                case DataModelType.service:
-                                case DataModelType.complaints:
-                                  return true;
-                                default:
-                                  return false;
-                              }
-                            }).length,
+                        return TextButton(
+                          style: TextButton.styleFrom(
+                            foregroundColor: theme.colorScheme.onPrimary,
                           ),
+                          onPressed: () {
+                            ctx.router.navigate(const BoundarySelectionRoute());
+                          },
+                          child: Text(boundaryName),
                         );
                       },
-                    );
+                    ),
+                  ],
+                ),
+                drawer: showDrawer ? const Drawer(child: SideBar()) : null,
+                body: MultiBlocProvider(
+                  providers: [
+                    BlocProvider(
+                      create: (context) {
+                        return SearchHouseholdsBloc(
+                          beneficiaryType: context.beneficiaryType,
+                          userUid: context.loggedInUserUuid,
+                          projectId: context.projectId,
+                          projectBeneficiary: context.repository<
+                              ProjectBeneficiaryModel,
+                              ProjectBeneficiarySearchModel>(),
+                          householdMember: context.repository<
+                              HouseholdMemberModel,
+                              HouseholdMemberSearchModel>(),
+                          household: context.repository<HouseholdModel,
+                              HouseholdSearchModel>(),
+                          individual: context.repository<IndividualModel,
+                              IndividualSearchModel>(),
+                          taskDataRepository:
+                              context.repository<TaskModel, TaskSearchModel>(),
+                        )..add(const SearchHouseholdsClearEvent());
+                      },
+                    ),
+                    BlocProvider(
+                      create: (context) {
+                        final userId = context.loggedInUserUuid;
 
-                    return bloc;
-                  },
-                ),
-                BlocProvider(
-                  create: (_) => LocationBloc(location: Location())
-                    ..add(const LoadLocationEvent()),
-                ),
-                BlocProvider(
-                  create: (_) =>
-                      HouseholdDetailsBloc(const HouseholdDetailsState()),
-                ),
-              ],
-              child: AutoRouter(
-                navigatorObservers: () => [
-                  AuthenticatedRouteObserver(
-                    onNavigated: () {
-                      bool shouldShowDrawer;
-                      switch (context.router.topRoute.name) {
-                        case ProjectSelectionRoute.name:
-                        case BoundarySelectionRoute.name:
-                          shouldShowDrawer = false;
-                          break;
-                        default:
-                          shouldShowDrawer = true;
-                      }
+                        final isar = context.read<Isar>();
+                        final bloc = SyncBloc(
+                          isar: isar,
+                          networkManager: context.read(),
+                        )..add(SyncRefreshEvent(userId));
 
-                      _drawerVisibilityController.add(shouldShowDrawer);
-                    },
+                        isar.opLogs
+                            .filter()
+                            .createdByEqualTo(userId)
+                            .syncedUpEqualTo(false)
+                            .watch()
+                            .listen(
+                          (event) {
+                            bloc.add(
+                              SyncRefreshEvent(
+                                userId,
+                                event.where((element) {
+                                  switch (element.entityType) {
+                                    case DataModelType.household:
+                                    case DataModelType.individual:
+                                    case DataModelType.task:
+                                    case DataModelType.householdMember:
+                                    case DataModelType.projectBeneficiary:
+                                    case DataModelType.stock:
+                                    case DataModelType.stockReconciliation:
+                                    case DataModelType.service:
+                                    case DataModelType.complaints:
+                                      return true;
+                                    default:
+                                      return false;
+                                  }
+                                }).length,
+                              ),
+                            );
+                          },
+                        );
+
+                        return bloc;
+                      },
+                    ),
+                    BlocProvider(
+                      create: (_) => LocationBloc(location: Location())
+                        ..add(const LoadLocationEvent()),
+                    ),
+                    BlocProvider(
+                      create: (_) =>
+                          HouseholdDetailsBloc(const HouseholdDetailsState()),
+                    ),
+                  ],
+                  child: AutoRouter(
+                    navigatorObservers: () => [
+                      AuthenticatedRouteObserver(
+                        onNavigated: () {
+                          bool shouldShowDrawer;
+                          switch (context.router.topRoute.name) {
+                            case ProjectSelectionRoute.name:
+                            case BoundarySelectionRoute.name:
+                              shouldShowDrawer = false;
+                              break;
+                            default:
+                              shouldShowDrawer = true;
+                          }
+
+                          _drawerVisibilityController.add(shouldShowDrawer);
+                        },
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
+                ),
+              );
+            }),
           ),
         );
       },
---
+++ apps/health_campaign_field_worker_app/lib/widgets/showcase/custom_showcase_widget.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:showcaseview/showcaseview.dart';
+
+class CustomShowcaseWrapper extends StatefulWidget {
+  final int showcasePathCount;
+  final Widget Function(
+    BuildContext context,
+    List<GlobalKey> showcaseKeys,
+  ) builder;
+
+  const CustomShowcaseWrapper({
+    super.key,
+    required this.showcasePathCount,
+    required this.builder,
+  });
+
+  @override
+  State<CustomShowcaseWrapper> createState() => _CustomShowcaseWrapperState();
+}
+
+class _CustomShowcaseWrapperState extends State<CustomShowcaseWrapper> {
+  late final List<GlobalKey> _showcaseKeys;
+
+  @override
+  void initState() {
+    _showcaseKeys = List.generate(
+      widget.showcasePathCount,
+      (index) => GlobalKey(debugLabel: 'showcase_${index + 1}'),
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.builder(context, _showcaseKeys);
+  }
+}
+
+class CustomShowcaseWidget extends StatelessWidget {
+  final GlobalKey showcaseKey;
+  final Widget child;
+  final String message;
+
+  const CustomShowcaseWidget({
+    super.key,
+    required this.showcaseKey,
+    required this.child,
+    required this.message,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Showcase.withWidget(
+      key: showcaseKey,
+      height: 100,
+      width: MediaQuery.of(context).size.width,
+      container: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Material(
+          color: Colors.transparent,
+          child: Text(
+            message,
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+        ),
+      ),
+      child: child,
+    );
+  }
+}
